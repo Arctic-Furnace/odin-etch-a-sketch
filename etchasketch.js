1082b3deb1eeb516947a7ff6326c5016f16a9c9f
Arctic-Furnace <richardjameshaddon@gmail.com>
2024-08-12 22:24:50 +0100

    Add a new grid button to reset the drawing area

--- etchasketch.js
+++ etchasketch.js
@@ -1,23 +1,43 @@
 console.log("Welcome to Etch-a sketch");
 
+let gridSize = 16;
+
 const grid = document.querySelector('#container');
 
-
-
-for(let i = 0; i < 16; i++ )
-{
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for(let i = 0; i < 16; i++ )
-    {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = "blue";
-        })
-        row.appendChild(square);
-        console.log("Adding square " + i);
-    }
-    grid.appendChild(row);
+function newGrid(gridSize){
+    for(let i = 0; i < gridSize; i++ )
+        {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            for(let i = 0; i < gridSize; i++ )
+            {
+                const square = document.createElement("div");
+                square.classList.add("square");
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = "blue";
+                })
+                row.appendChild(square);
+                console.log("Adding square " + i);
+            }
+            grid.appendChild(row);
+        }
 }
 
+
+newGrid(gridSize);
+
+const buttonNewSketch = document.getElementById("buttonNewSketch");
+
+buttonNewSketch.addEventListener('click', () => {
+
+    gridSize = window.prompt("What size grid would you like? Max 100");
+    if(gridSize > 99)
+    {
+        gridSize = 100;
+    }
+    
+    while (grid.lastElementChild){
+        grid.removeChild(grid.lastElementChild);
+    }
+    newGrid(gridSize);
+})
