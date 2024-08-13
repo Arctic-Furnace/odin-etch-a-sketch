f1c85f9666a8d85688cbee38ecd6027c71f54599
Arctic-Furnace <richardjameshaddon@gmail.com>
2024-08-13 21:38:45 +0100

    Add buttons to change ink colour

--- etchasketch.js
+++ etchasketch.js
@@ -2,7 +2,14 @@ console.log("Welcome to Etch-a sketch");
 
 let gridSize = 16;
 
+let paintColor = "black"
+
 const grid = document.querySelector('#container');
+const buttonRainbow = document.getElementById("buttonRainbow");
+const buttonBlack = document.getElementById("buttonBlack");
+const buttonBlue = document.getElementById("buttonBlue");
+
+buttonBlack.style.backgroundColor = "LightGreen";
 
 function newGrid(gridSize){
     for(let i = 0; i < gridSize; i++ )
@@ -14,8 +21,13 @@ function newGrid(gridSize){
                 const square = document.createElement("div");
                 square.classList.add("square");
                 square.addEventListener('mouseover', () => {
-
-                    square.style.backgroundColor =  "#" + Math.floor(Math.random() * 16777215).toString(16) ;
+                    if(paintColor == "rainbow"){
+                        square.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16) ;
+                    }
+                    else{
+                        square.style.backgroundColor =  paintColor;
+                    }
+                    
                 })
                 row.appendChild(square);
                 console.log("Adding square " + i);
@@ -42,3 +54,31 @@ buttonNewSketch.addEventListener('click', () => {
     }
     newGrid(gridSize);
 })
+
+
+
+buttonBlue.addEventListener('click', () => {
+    buttonBlue.style.backgroundColor = "LightGreen";
+    buttonBlack.style.backgroundColor = "";
+    buttonRainbow.style.backgroundColor = "";
+    paintColor = "blue";
+})
+
+
+
+buttonBlack.addEventListener('click', () => {
+    buttonBlue.style.backgroundColor = "";
+    buttonBlack.style.backgroundColor = "LightGreen";
+    buttonRainbow.style.backgroundColor = "";
+    paintColor = "black";
+})
+
+
+
+buttonRainbow.addEventListener('click', () => {
+    buttonBlue.style.backgroundColor = "";
+    buttonBlack.style.backgroundColor = "";
+    buttonRainbow.style.backgroundColor = "LightGreen";
+    paintColor = "rainbow";
+})
+
